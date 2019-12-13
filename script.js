2cb41801d0bf6d5df9c234636398760a2406ec87
Jonathan Goldfuss <jgoldfuss@coproductions.com>
2019-12-13 13:48:43 -0500

    panel hide is working

--- script.js
+++ script.js
@@ -4,40 +4,27 @@ const developerPanel = document.querySelector('#developer');
 const marketerPanel = document.querySelector('#marketer');
 const productPanel = document.querySelector('#product');
 
-// function openPanel(id, display) {
-//   display = 'block';
-// }
-
-// function closePanel(id)
-
 const sectionHeaders = document.querySelector('.container');
 
 sectionHeaders.addEventListener('click', function(evt) {
   let id = evt.target.id;
-  let display = evt.target.style.display;
-  // console.log(evt);
-  // console.log(display);
+  console.log(evt);
   console.log(id);
-  if (display !== 'block') {
-    if (id === 'b') {
-      brandPanel.style.display = 'block';
-    } else if (id === 'f') {
-      founderPanel.style.display = 'block';
-    } else if (id === 'd') {
-      developerPanel.style.display = 'block';
-    } else if (id === 'm') {
-      marketerPanel.style.display = 'block';
-    } else if (id === 'p') {
-      productPanel.style.display = 'block';
-    }
+  if (id === 'b') {
+    brandPanel.style.display = 'block';
+  } else if (id === 'f') {
+    founderPanel.style.display = 'block';
+  } else if (id === 'd') {
+    developerPanel.style.display = 'block';
+  } else if (id === 'm') {
+    marketerPanel.style.display = 'block';
+  } else if (id === 'p') {
+    productPanel.style.display = 'block';
   } else {
-    display = 'none';
+    brandPanel.style.display = '';
+    founderPanel.style.display = '';
+    developerPanel.style.display = '';
+    marketerPanel.style.display = '';
+    productPanel.style.display = '';
   }
-  // if (id) {
-  //   switch (id) {
-  //     case 'b':
-  //       brandPanel.style.display = 'block';
-  //       break;
-  //   }
-  // }
 });
